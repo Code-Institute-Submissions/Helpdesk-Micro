34247fafbb50a13933f2342400a15354fbac9ac5
pengols <pengelly30@hotmail.com>
2020-08-26 19:12:00 +0000

    linked mongodb test db using env.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+from os import path
+if path.exists("env.py"):
+  import env 
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["MONGO_DBNAME"] = "HelpdeskDB"
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello Werld'
+@app.route('/get_tickets')
+def get_tickets():
+    return render_template('tickets.html', tickets=mongo.db.tickets.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ["MONGO_URI"] = "mongodb+srv://pengols:Nochance@myfirstcluster.hidiq.mongodb.net/HelpdeskDB?retryWrites=true&w=majority"
