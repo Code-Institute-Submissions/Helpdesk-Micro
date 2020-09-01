28490b9c2201c286a8e301198eed786228043e58
pengols <pengelly30@hotmail.com>
2020-09-01 18:56:40 +0000

    fixed path to local scripts.js and added navbar js to scripts.js

---
+++ static/js/scripts.js
@@ -0,0 +1,5 @@
+// Navbar collapes and add to right of screen on small screens 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {edge:'right'});
+  });
