bc9998af4477e8eec5c4b81597a5e45f74031a5b
pengols <pengelly30@hotmail.com>
2020-10-02 22:06:27 +0000

    template colour coding fr call priority added to collapsible headers

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -9,10 +9,16 @@ $(document).ready(function () {
   $(".collapsible").collapsible(); /* accordion */
   $("select").formSelect(); /* select dropdowns */
   $(".modal").modal(); /* delete user confirmation */
+  $(".collapsible-header:contains('High')").addClass('red'); /* adds class to collapsible header */
+  $(".collapsible-header:contains('Medium')").addClass('blue');
+  $(".collapsible-header:contains('Low')").addClass('green');
+  $(".collapsible-header:contains('Information')").addClass('yellow');
 });
+
+
 
 // random background image on page refresh
 $(function() {
-    var images = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg'];
+    var images = ['bg1.jpg', 'bg2.jpg', 'bg3.jpg', 'bg4.jpg'];
     $('.bg-img').css({'background': 'url(/static/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
    });
