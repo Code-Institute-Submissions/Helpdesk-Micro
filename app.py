7a593e14d90972e87e2bd60ad19f94940b246967
pengols <pengelly30@hotmail.com>
2020-11-11 23:04:37 +0000

    renamed sequence from messages for insert new ticket

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def insert_ticket():
                   'end_user': request.form.get('end_user'),
                   'eu_email': request.form.get('eu_email'),
                   'tel_no': request.form.get('tel_no'),
-                  '_ticketid': get_sequence('messages'), # inserts the incremented ticketid from get_sequence function
+                  '_ticketid': get_sequence('sequence'), # inserts the incremented ticketid from get_sequence function
                   'admin_username': request.form.get('admin_username'),
                   }
 
