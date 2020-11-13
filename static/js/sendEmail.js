7bf01a9c3f9cc79fe271ff7fe81fd2a549bedcbc
pengols <pengelly30@hotmail.com>
2020-11-13 17:05:52 +0000

    added sendemail.js and linked contact form to send emails

---
+++ static/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(aboutForm) {
+    emailjs.send("gmail", "template_p67sxbc", {
+        "from_name": aboutForm.from_name.value,
+        "reply_to": aboutForm.reply_to.value,
+        "message": aboutForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
