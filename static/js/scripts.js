47ddcbb3132924dc67e68e6d6c33b6c1bdca3afa
pengols <pengelly30@hotmail.com>
2020-11-14 22:22:31 +0000

    ran beautify command on code to tidy

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -12,7 +12,9 @@ $(document).ready(function () {
   $(".collapsible-header:contains('Priority: High')").addClass(
     "priority-high"
   ); /* adds class to collapsible header */
-  $(".collapsible-header:contains('Priority: Medium')").addClass("priority-med");
+  $(".collapsible-header:contains('Priority: Medium')").addClass(
+    "priority-med"
+  );
   $(".collapsible-header:contains('Priority: Low')").addClass("priority-low");
   $(".collapsible-header:contains('Priority: Info')").addClass("priority-info");
   $(".collapsible-header:contains('Status: Closed')").addClass("closed-ticket");
@@ -21,10 +23,32 @@ $(document).ready(function () {
 
 // random background image on page refresh
 
-$(function() {
-    var images = ["bg1.jpg", "bg2.jpg", "bg3.jpg", "bg4.jpg", "bg5.jpg", "bg6.jpg", "bg7.jpg", "bg8.jpg", "bg9.jpg", "bg10.jpg", "bg11.jpg", "bg12.jpg", "bg13.jpg", "bg14.jpg", "bg15.jpg", "bg16.jpg"];
-    $('body').css({'background-image': 'url(/static/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
-   });
+$(function () {
+  var images = [
+    "bg1.jpg",
+    "bg2.jpg",
+    "bg3.jpg",
+    "bg4.jpg",
+    "bg5.jpg",
+    "bg6.jpg",
+    "bg7.jpg",
+    "bg8.jpg",
+    "bg9.jpg",
+    "bg10.jpg",
+    "bg11.jpg",
+    "bg12.jpg",
+    "bg13.jpg",
+    "bg14.jpg",
+    "bg15.jpg",
+    "bg16.jpg",
+  ];
+  $("body").css({
+    "background-image":
+      "url(/static/images/" +
+      images[Math.floor(Math.random() * images.length)] +
+      ")",
+  });
+});
 
 // disables submit buttons on submit & stops multiple clicks and duplicate forms being created
 $("body").on("submit", "form", function () {
